eb02b53ff9924a7124209bc8213993643d1a55bd
Max Prettyjohns <heshoots9999@gmail.com>
2017-07-05 11:27:19 +0100

    Remove old WebGL Globe API's

--- queries.js
+++ queries.js
@@ -11,23 +11,9 @@ var pgp = require('pg-promise')(options);
 var connectionString = require('./connectionstring.js');
 var db = pgp(connectionString);
 
-function getReadingsDate(req, res, next) {
-  console.log(req.params.date);
-  db.many("SELECT lat, long, value FROM station_values LEFT JOIN (SELECT SUM(value) as value, station FROM time_values WHERE timestamp <= ('" + req.params.date + "'::date + '1 day'::interval) AND timestamp >= '" + req.params.date + "'::date GROUP BY (station)) as db ON (db.station = station_values.station)")
-    .then((data, test) => {
-      console.log(data, test);
-      res.status(200)
-        .json({
-          status: 'success',
-          data,
-          message: 'gotReading'
-        });
-    });
-}
-
 function getReadingsDateTime(req, res, next) {
   const timestamp = `${req.params.date} ${moment(req.params.time, 'hh-mm').format('hh:mm:ss')}`;
-  db.manyOrNone("SELECT lat, long, value FROM station_values RIGHT JOIN (SELECT value, station FROM time_values WHERE value != 0 AND timestamp >= '" + timestamp + "' AND timestamp <= '" + timestamp +"'::timestamp + INTERVAL '15 minutes') as db ON station_values.station = db.station")
+  db.manyOrNone(`SELECT lat, long, value FROM station_values RIGHT JOIN (SELECT value, station FROM time_values WHERE value != 0 AND timestamp >= '${timestamp}' AND timestamp <= '${timestamp}'::timestamp + INTERVAL '15 minutes') as db ON station_values.station = db.station`)
     .then((data) => {
       res.status(200)
         .json({
@@ -36,48 +22,25 @@ function getReadingsDateTime(req, res, next) {
           message: 'gotReading'
         });
     });
-}
-
-
-function getReadingsDateTimeGlobe(req, res, next) {
-  const timestamp = `${req.params.date} ${moment(req.params.time, 'hh-mm').format('hh:mm:ss')}`;
-  db.manyOrNone("SELECT lat, long, value FROM station_values RIGHT JOIN (SELECT value, station FROM time_values WHERE value != 0 AND timestamp >= '" + timestamp + "' AND timestamp <= '" + timestamp +"'::timestamp + INTERVAL '15 minutes') as db ON station_values.station = db.station")
-    .then((data) => {
-      let arraydata = data.map(point => ([point.lat, point.long, point.value]));
-      let outData = [];
-      for (let i = 0; i < arraydata.length; i++) {
-        outData.push(arraydata[i][0]);
-        outData.push(arraydata[i][1]);
-        outData.push(arraydata[i][2]);
-      }
-      out = [
-        [
-          timestamp, outData
-        ]
-      ];
-      res.status(200)
-        .json(
-          out
-        );
-    });
-}
-
-function getReadingsWeek(req, res, next) {
-  const timestamp = `${req.params.date}`
-  const countPromise = db.manyOrNone(`select timestamp, count(*) from time_values where (value != 0 AND timestamp >= '${timestamp}'::date AND timestamp <= '${timestamp}'::date + INTERVAL '1 day') group by timestamp ORDER BY timestamp;`)
-  const valuePromise = db.manyOrNone(`select * from time_values where (value != 0 AND timestamp >= '${timestamp}'::date AND timestamp <= '${timestamp}'::date + INTERVAL '1 day') ORDER BY timestamp, id;`)
-  promise.all([valuePromise, countPromise], (data) =>
-    res.status(200)
-      .json({
-        status: 'success',
-        data: data,
-        message: 'gotReading'
-      }));
 }
 
 function getLevelsDateTime(req, res, next) {
   const timestamp = `${req.params.date} ${moment(req.params.time, 'hh-mm').format('hh:mm:ss')}`;
-  db.manyOrNone(`SELECT lat, long, val / divisor as percentage FROM (SELECT timestamp, lat, long, (value - avgvalue) as val, (typicalmax - avgvalue) as divisor, avgvalue, value from level_values  LEFT JOIN level_station_values ON (level_values.station = level_station_values.station)) as perc where value > avgvalue AND divisor > 0 AND timestamp >= '${timestamp}'::timestamp AND timestamp < '${timestamp}'::timestamp + INTERVAL '15 minutes' ORDER by percentage`)
+  db.manyOrNone(`SELECT areas.lat, areas.long, sum(value)/count(*) as value from areas RIGHT JOIN (SELECT lat, long, val / divisor as value FROM (SELECT timestamp, lat, long, (value - avgvalue) as val, (typicalmax - avgvalue) as divisor, avgvalue, value from level_values RIGHT JOIN level_station_values ON (level_values.station = level_station_values.station)) as perc where value > avgvalue AND divisor > 0 AND timestamp >= '${timestamp}'::timestamp AND timestamp < '${timestamp}'::timestamp + INTERVAL '15 minutes') as lv on (areas.lat <= lv.lat AND areas.lat + 0.1 > lv.lat AND areas.long <= lv.long AND areas.long + 0.2 > lv.long) WHERE areas.lat is not null group by areas.lat, areas.long`)
+ // db.manyOrNone(`SELECT lat, long, val / divisor as value FROM (SELECT timestamp, lat, long, (value - avgvalue) as val, (typicalmax - avgvalue) as divisor, avgvalue, value from level_values  LEFT JOIN level_station_values ON (level_values.station = level_station_values.station)) as perc where divisor > 0 AND timestamp >= '${timestamp}'::timestamp AND timestamp < '${timestamp}'::timestamp + INTERVAL '15 minutes'`)
+    .then((data) => {
+      res.status(200)
+        .json({
+          status: 'success',
+          data,
+          message: 'gotReading'
+        });
+    });
+}
+
+function getTideDateTime(req, res, next) {
+  const timestamp = `${req.params.date} ${moment(req.params.time, 'hh-mm').format('hh:mm:ss')}`;
+  db.manyOrNone(`SELECT lat, long, val / divisor as value FROM (SELECT timestamp, lat, long, (value - typicalmin) as val, (typicalmax - typicalmin) as divisor, avgvalue, value from tide_values LEFT JOIN tide_station_values ON (tide_values.station = tide_station_values.station)) as perc where divisor > 0 AND timestamp >= '${timestamp}'::timestamp AND timestamp < '${timestamp}'::timestamp + INTERVAL '15 minutes'`)
     .then((data) => {
       res.status(200)
         .json({
@@ -89,9 +52,7 @@ function getLevelsDateTime(req, res, next) {
 }
 
 module.exports = {
-  getReadingsDate: getReadingsDate,
   getReadingsDateTime: getReadingsDateTime,
-  getReadingsDateTimeGlobe: getReadingsDateTimeGlobe,
-  getReadingsWeek: getReadingsWeek,
   getLevelsDateTime: getLevelsDateTime,
+  getTideDateTime: getTideDateTime,
 };
--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/reading/:date', db.getReadingsDate);
 router.get('/api/reading/:date/:time', db.getReadingsDateTime);
-router.get('/api/globe/:date/:time', db.getReadingsDateTimeGlobe);
-router.get('/api/readingweek/:date/', db.getReadingsWeek);
 router.get('/api/levels/:date/:time', db.getLevelsDateTime);
+router.get('/api/tide/:date/:time', db.getTideDateTime);
 
 module.exports = router;

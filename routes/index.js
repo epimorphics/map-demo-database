8f9818ee9447e844e928c32d8af3e2c5143cb86e
Max Prettyjohns <heshoots9999@gmail.com>
2017-07-05 14:48:51 +0100

    Remove unnecessary routes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../queries.js');
 
-/* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
-});
-
 router.get('/api/reading/:date/:time', db.getReadingsDateTime);
 router.get('/api/levels/:date/:time', db.getLevelsDateTimeAreas);
 router.get('/api/tide/:date/:time', db.getTideDateTime);

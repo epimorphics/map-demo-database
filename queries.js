34dfc4b3adb9ee5da4313db6fc7186ba683e0971
Max Prettyjohns <heshoots9999@gmail.com>
2017-07-06 10:34:04 +0100

    Further Query cleanup

--- queries.js
+++ queries.js
@@ -6,11 +6,62 @@ const options = {
 };
 
 const pgp = require('pg-promise')(options);
+const PS = require('pg-promise').PreparedStatement;
 
 // Modify connectionstring.js.example
 const connectionString = require('./connectionstring.js');
-
 const db = pgp(connectionString);
+
+// Database Queries
+const getReadings = new PS('get-readings',
+  'SELECT lat, long, value FROM station_values \
+   RIGHT JOIN (SELECT value, station FROM time_values \
+   WHERE value != 0 AND \
+   timestamp >= $1 \
+   AND timestamp <= $1::timestamp + INTERVAL \'15 minutes\') as db \
+   ON station_values.station = db.station');
+
+const getLevelsAreas = new PS('get-levels-areas',
+  'SELECT areas.lat, areas.long, sum(value)/count(*) as value from areas \
+   RIGHT JOIN \
+   (SELECT lat, long, val / divisor as value FROM \
+      (SELECT timestamp, lat, long, \
+        (value - avgvalue) as val, \
+        (typicalmax - avgvalue) as divisor, \
+        avgvalue, value from level_values \
+        RIGHT JOIN level_station_values \
+        ON (level_values.station = level_station_values.station)) as perc \
+      where value > avgvalue \
+      AND divisor > 0 \
+      AND timestamp >= $1::timestamp \
+      AND timestamp < $1::timestamp + INTERVAL \'15 minutes\') as lv \
+   ON (areas.lat <= lv.lat \
+       AND areas.lat + 0.1 > lv.lat \
+       AND areas.long <= lv.long \
+       AND areas.long + 0.2 > lv.long) \
+   WHERE areas.lat is not null group by areas.lat, areas.long');
+
+const getLevels = new PS('get-levels',
+  'SELECT lat, long, val / divisor as value \
+   FROM (SELECT timestamp, lat, long, avgvalue, value, \
+   (value - avgvalue) as val, (typicalmax - avgvalue) as divisor \
+   from level_values \
+   LEFT JOIN level_station_values \
+   ON (level_values.station = level_station_values.station)) as perc \
+   WHERE divisor > 0 AND timestamp >= $1::timestamp \
+   AND timestamp < $1::timestamp + INTERVAL \'15 minutes\'');
+
+const getTide = new PS('get-tide',
+  'SELECT lat, long, val / divisor as value \
+   FROM (SELECT timestamp, lat, long, avgvalue, value, \
+     (value - typicalmin) as val, \
+     (typicalmax - typicalmin) as divisor \
+     FROM tide_values \
+     LEFT JOIN tide_station_values \
+     ON (tide_values.station = tide_station_values.station)) as perc \
+   WHERE divisor > 0 \
+   AND timestamp >= $1::timestamp \
+   AND timestamp < $1::timestamp + INTERVAL \'15 minutes\'')
 
 /**
  * Validates date and time supplied in URL
@@ -29,22 +80,19 @@ function validateDateTime(date, time) {
 }
 
 /**
- * Gets Rainfall data for given date and time within 15 minute interval
+ * Most queries simply take a date time string, valides it and then applies
+ * this to a database query. This helper function performs this task for
+ * multiple different queries
  *
- * @param {} req HTTP request
- * @param {} res HTTP response
+ * @param {PreparedStatement} query takes one parameter, a timestamp string
+ * @param {} req HTTP Request
+ * @param {} res HTTP Response
  */
-function getReadingsDateTime(req, res) {
+function getGenericDataDateTime(query, req, res) {
   validateDateTime(req.params.date, req.params.time)
     .then((timestamp) => {
-      db.manyOrNone(
-        `SELECT lat, long, value FROM station_values \
-         RIGHT JOIN (SELECT value, station FROM time_values \
-         WHERE value != 0 AND \
-         timestamp >= '${timestamp}' \
-         AND timestamp <= '${timestamp}'::timestamp + INTERVAL '15 minutes') as db \
-         ON station_values.station = db.station`
-      )
+      query.values = [timestamp];
+      db.manyOrNone(query)
         .then((data) => {
           res.status(200)
             .json({
@@ -62,125 +110,51 @@ function getReadingsDateTime(req, res) {
           message: 'Invalid date',
         });
     });
+}
+
+/**
+ * Gets Rainfall data for given date and time within 15 minute interval
+ */
+function getReadingsDateTime(req, res) {
+  getGenericDataDateTime(getReadings, req, res);
 }
 
 /**
  * Gets river levels for a given date and time within 15 minute interval for
  * Discrete areas of 0.1degrees latitudinally and 0.2 degrees longitudinally
- *
- * @param {} req HTTP request
- * @param {} res HTTP response
  */
 function getLevelsDateTimeAreas(req, res) {
-  validateDateTime(req.params.date, req.params.time)
-    .then((timestamp) => {
-      db.manyOrNone(
-        `SELECT areas.lat, areas.long, sum(value)/count(*) as value from areas \
-         RIGHT JOIN \
-         (SELECT lat, long, val / divisor as value FROM \
-            (SELECT timestamp, lat, long, \
-              (value - avgvalue) as val, \
-              (typicalmax - avgvalue) as divisor, \
-              avgvalue, value from level_values \
-              RIGHT JOIN level_station_values \
-              ON (level_values.station = level_station_values.station)) as perc \
-            where value > avgvalue \
-            AND divisor > 0 \
-            AND timestamp >= '${timestamp}'::timestamp \
-            AND timestamp < '${timestamp}'::timestamp + INTERVAL '15 minutes') as lv \
-         ON (areas.lat <= lv.lat \
-             AND areas.lat + 0.1 > lv.lat \
-             AND areas.long <= lv.long \
-             AND areas.long + 0.2 > lv.long) \
-         WHERE areas.lat is not null group by areas.lat, areas.long`
-      )
-        .then((data) => {
-          res.status(200)
-            .json({
-              status: 'success',
-              data,
-              message: 'gotReading',
-            });
-        });
-    })
-    .catch(() => {
-      res.status(404)
-        .json({
-          status: 'failure',
-          data: [],
-          message: 'Invalid date',
-        });
-    });
+  getGenericDataDateTime(getLevelsAreas, req, res);
 }
 
 /**
  * Gets river levels for a given date and time within 15 minute interval
- *
- * @param {} req HTTP request
- * @param {} res HTTP response
  */
 function getLevelsDateTime(req, res) {
-  validateDateTime(req.params.date, req.params.time)
-    .then((timestamp) => {
-      db.manyOrNone(
-        `SELECT lat, long, val / divisor as value \
-         FROM (SELECT timestamp, lat, long, (value - avgvalue) as val, (typicalmax - avgvalue) as divisor, avgvalue, value from level_values  LEFT JOIN level_station_values ON (level_values.station = level_station_values.station)) as perc where divisor > 0 AND timestamp >= '${timestamp}'::timestamp AND timestamp < '${timestamp}'::timestamp + INTERVAL '15 minutes'`
-      )
-        .then((data) => {
-          res.status(200)
-            .json({
-              status: 'success',
-              data,
-              message: 'gotReading',
-            });
-        });
-    })
-    .catch(() => {
-      res.status(404)
-        .json({
-          status: 'failure',
-          data: [],
-          message: 'Invalid date',
-        });
-    });
+  getGenericDataDateTime(getLevelsAreas, req, res);
 }
 
 /**
  * Gets tidal data for a given date and time within 15 minute interval
- *
- * @param {} req HTTP request
- * @param {} res HTTP response
  */
 function getTideDateTime(req, res) {
-  validateDateTime(req.params.date, req.params.time)
-    .then((timestamp) => {
-      db.manyOrNone(
-        `SELECT lat, long, val / divisor as value \
-         FROM (SELECT timestamp, lat, long, avgvalue, value, \
-           (value - typicalmin) as val, \
-           (typicalmax - typicalmin) as divisor \
-           FROM tide_values \
-           LEFT JOIN tide_station_values \
-           ON (tide_values.station = tide_station_values.station)) as perc \
-         WHERE divisor > 0 \
-         AND timestamp >= '${timestamp}'::timestamp \
-         AND timestamp < '${timestamp}'::timestamp + INTERVAL '15 minutes'`
-      )
-        .then((data) => {
-          res.status(200)
-            .json({
-              status: 'success',
-              data,
-              message: 'gotReading',
-            });
-        });
-    })
-    .catch(() => {
-      res.status(404)
+  getGenericDataDateTime(getTide, req, res);
+}
+
+function getStations(req, res) {
+  db.manyOrNone(
+    `SELECT station, lat, long FROM station_values
+     UNION SELECT station, lat, long FROM level_station_values
+     UNION SELECT station, lat, long FROM tide_station_values
+     ORDER BY station
+    `
+  )
+    .then((data) => {
+      res.status(200)
         .json({
-          status: 'failure',
-          data: [],
-          message: 'Invalid date',
+          status: 'success',
+          data,
+          message: 'gotReading',
         });
     });
 }
